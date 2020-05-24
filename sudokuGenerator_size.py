448c9a1384879cb75e015653a7fcdb660fc2d2df
Alvaro <alvarovisus@gmail.com>
2020-05-24 12:45:44 +0200

    Sudokus

--- sudokuGenerator_size.py
+++ sudokuGenerator_size.py
@@ -5,32 +5,33 @@ import os
 
 path = 'py-sudokus/Size/'
 
-def generateSudoku(x, y):
+def generateSudoku(x, y, z):
     n = int(x)*int(y)
     size_folder = path +x+ 'x'+ y
     if not os.path.exists(size_folder):
         os.mkdir(size_folder)
-    for i in range(1, 101):
-        puzzle = Sudoku(int(x),int(y), seed=i).difficulty(0.66)
-        puzzle.show()
-        filename = 'Sudoku'+str(i)+"_"+x+"x"+y
-        final_path = os.path.join(size_folder, filename)
-        with open(final_path, 'w') as f:
-            for row in range(n):
-                for col in range(n):
-                    value = puzzle.board[row][col]
-                    if value == None: value = 0
-                    print(value,file=f, end="")
-                    if col == n - 1:
-                        print(file=f, end="\n")
-                    else:
-                        print(file=f, end=" ")
+    for i in range(1, z):
+        if(not(x==3 and y == 5 and i == 52)):
+            puzzle = Sudoku(int(x),int(y), seed=i).difficulty(0.66)
+            puzzle.show()
+            filename = 'Sudoku'+str(i)+"_"+x+"x"+y
+            final_path = os.path.join(size_folder, filename)
+            with open(final_path, 'w') as f:
+                for row in range(n):
+                    for col in range(n):
+                        value = puzzle.board[row][col]
+                        if value == None: value = 0
+                        print(value,file=f, end="")
+                        if col == n - 1:
+                            print(file=f, end="\n")
+                        else:
+                            print(file=f, end=" ")
 
-generateSudoku('2', '2')
-generateSudoku('2', '3')
-generateSudoku('2', '4')
-generateSudoku('3', '3')
-generateSudoku('2', '5')
-generateSudoku('3', '4')
-generateSudoku('3', '5')
-generateSudoku('4', '4')
+generateSudoku('2', '2', 101)
+generateSudoku('2', '3', 101)
+generateSudoku('2', '4', 101)
+generateSudoku('3', '3', 101)
+generateSudoku('2', '5', 101)
+generateSudoku('3', '4', 101)
+generateSudoku('3', '5', 51)
+generateSudoku('4', '4', 51)
